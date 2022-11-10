b9a78145b6f801ee846092cb04a71135051675eb
febin123 <febsep96@gmail.com>
2022-11-10 06:52:00 +0530

    search bar logic added

--- app.js
+++ app.js
@@ -23,3 +23,36 @@ window.onscroll = () =>{
 
 }
 /* end of logic*/
+/*start ofsearch bar */
+let filterInput=document.getElementById("search-data");
+filterInput.addEventListener('click',filterName)
+
+function filterName(){
+  const cl=document.getElementById("sales");
+  cl.scrollIntoView({behavior:"smooth"})
+}
+filterInput.addEventListener('keyup',filterNames)
+
+function filterNames(){
+  const searchbox=document.getElementById("search-data").value.toUpperCase();
+    const storeitems=document.getElementById("box-container")
+  const product=document.querySelectorAll(".box")
+  const pname=document.getElementsByTagName("h3")
+
+
+    for(var i=0;i<pname.length;i++){
+      let match=product[i].getElementsByTagName('h3')[0];
+
+      if(match){
+        let textvalue=match.textContent || match.innerHTML;
+
+        if(textvalue.toUpperCase().indexOf(searchbox)>-1){
+          product[i].style.display="";
+        }else{
+          product[i].style.display="none";
+        }
+      }
+  }
+
+}
+/*end of search bar */
